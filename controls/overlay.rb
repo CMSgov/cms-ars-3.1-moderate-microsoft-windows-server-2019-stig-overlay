d82a96900f9510740ec05676cf5fe3bbcdc0a4c0
karikarshivani <karikarshivani@gmail.com>
2020-08-18 15:38:26 -0400

    Updated the spreadsheet and the overlay file. Thanks @ejaronne

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -607,7 +607,7 @@ include_controls "microsoft-windows-server-2019-stig-baseline" do
 
   control "V-93379" do
     impact 0.0
-    desc "caveat", "This is Not Applicable since the related security control is not mandatory in CMS ARS 3.1"
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not mandatory  for this security categorization in CMS ARS 3.1"
   end
 
   control "V-93425" do
@@ -813,37 +813,37 @@ include_controls "microsoft-windows-server-2019-stig-baseline" do
 
   control "V-93517" do
     impact 0.0
-    desc "caveat", "This is Not Applicable since the related security control is not included in CMS ARS 3.1"
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1 for this system categorization"
   end
 
   control "V-93519" do
     impact 0.0
-    desc "caveat", "This is Not Applicable since the related security control is not included in CMS ARS 3.1"
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1 for this system categorization"
   end
 
   control "V-93521" do
     impact 0.0
-    desc "caveat", "This is Not Applicable since the related security control is not included in CMS ARS 3.1"
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1 for this system categorization"
   end
 
   control "V-93523" do
     impact 0.0
-    desc "caveat", "This is Not Applicable since the related security control is not included in CMS ARS 3.1"
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1 for this system categorization"
   end
 
   control "V-93525" do
     impact 0.0
-    desc "caveat", "This is Not Applicable since the related security control is not included in CMS ARS 3.1"
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1 for this system categorization"
   end
 
   control "V-93527" do
     impact 0.0
-    desc "caveat", "This is Not Applicable since the related security control is not included in CMS ARS 3.1"
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1 for this system categorization"
   end
 
   control "V-93529" do
     impact 0.0
-    desc "caveat", "This is Not Applicable since the related security control is not included in CMS ARS 3.1"
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1 for this system categorization"
   end
 
   control "V-93543" do
